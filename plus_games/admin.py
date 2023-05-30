ad9fec65938191196e51bb166d56d74e648d5829
Keven Andrade <nivekandrade@live.com>
2023-05-30 10:28:14 -0300

    add display admin

--- plus_games/admin.py
+++ plus_games/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from plus_games.models import Jogo
 
-# Register your models here.
+
+class Jogos(admin.ModelAdmin):
+    list_display = ['id', 'codigo_jogo', 'nome', 'plataforma_jogo', 'genero_jogo', 'classificacao', 'preco']
+    list_display_links = ['id', 'codigo_jogo', 'nome', ]
+    list_filter = ['plataforma_jogo', 'genero_jogo',  'classificacao']
+    search_fields = ['nome', 'plataforma_jogo']
+    list_per_page = 10
+    
+admin.site.register(Jogo, Jogos)
