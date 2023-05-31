a6ffd83f9b65e9b04e2ba7c1eb1d1e343b5c7a47
Keven Andrade <nivekandrade@live.com>
2023-05-31 20:21:19 -0300

    adic filtros em JogosViewSet

--- plus_games/views.py
+++ plus_games/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from plus_games.models import Jogo
 from plus_games.serializers import JogoSerializer
@@ -8,3 +9,7 @@ class JogosViewSet(viewsets.ModelViewSet):
     '''Exibindo todos os Jogos Cadastrados'''
     queryset = Jogo.objects.all()
     serializer_class = JogoSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['nome', 'plataforma_jogo', 'preco']
+    search_fields = ['codigo_jogo', 'nome', 'distribuidora']
+    filterset_fields = ['plataforma_jogo', 'genero_jogo', 'classificacao']
