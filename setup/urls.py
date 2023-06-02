8299e58fe23be91bbc455d84f5708cbb2f100465
Keven Andrade <nivekandrade@live.com>
2023-06-02 16:01:53 -0300

    adic media url

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls import static
 
 from plus_games.views import JogosViewSet
 
@@ -12,3 +14,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
